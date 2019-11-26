d90ca016baf0e03a6d68a20bfa82f33351214b71
zafaraabid <zafaraabid3295@gmail.com>
2019-11-26 10:18:52 +0530

    ability to analyse and respond Sad Mood

--- src/main/java/com/junit/MoodAnalyser.java
+++ src/main/java/com/junit/MoodAnalyser.java
@@ -2,6 +2,6 @@ package com.junit;
 public class MoodAnalyser {
     public String analyseMood(String im_feeling_happy) {
 
-        return "HAPPY";
+        return "SAD";
     }
 }
--- src/test/java/com/junit/MoodAnalyserTests.java
+++ src/test/java/com/junit/MoodAnalyserTests.java
@@ -7,7 +7,7 @@ public class MoodAnalyserTests {
     @Test
     public void givenMsg_shouldReturnSad_whenItsSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood=moodAnalyser.analyseMood("im feeling Happy");
-        Assert.assertEquals("HAPPY",mood);
+        String mood=moodAnalyser.analyseMood("im feeling SAD");
+        Assert.assertEquals("SAD",mood);
     }
 }
